6a78634f377edf5f8f49aa5020b2a2bc1790ffbd
BlackBarman <74059223+BlackBarman@users.noreply.github.com>
2023-02-01 10:28:49 +0100

    exercises
    exercises

---
+++ Taxes.py
@@ -0,0 +1,6 @@
+netto= 3000   # "=" gives value to something 
+tax =  netto * 0.21
+total = netto - tax
+print(type(total))
+#ciaoooo sono un commento 
+
--- hello.py
+++ hello.py
@@ -1,1 +1,3 @@
 print("spamma tua mamma")
+res =  (5+6+9)
+print(res)
